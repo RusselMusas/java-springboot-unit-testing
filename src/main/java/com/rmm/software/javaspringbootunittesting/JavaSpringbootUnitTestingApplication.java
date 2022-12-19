9392c2d5244e311b9946f19b3ace04cff19c5893
Russel Musasa <russelmusasa@gmail.com>
2022-12-19 18:02:50 +0100

    first commit

---
+++ src/main/java/com/rmm/software/javaspringbootunittesting/JavaSpringbootUnitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.rmm.software.javaspringbootunittesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootUnitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootUnitTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmm/software/javaspringbootunittesting/JavaSpringbootUnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmm.software.javaspringbootunittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootUnitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
