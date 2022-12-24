b0cdb415c5d21bd88ef66e16599005f428440cc0
Russel Musasa <russelmusasa@gmail.com>
2022-12-24 21:02:31 +0100

    Using Spring Mock MVC - Write Unit Test for the RestController

---
+++ src/test/java/com/rmm/software/javaspringbootunittesting/restcontroller/HelloWorldRestControllerTest.java
@@ -0,0 +1,36 @@
+package com.rmm.software.javaspringbootunittesting.restcontroller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+// Spring Unit Test
+@WebMvcTest(HelloWorldRestController.class)
+class HelloWorldRestControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	void helloWorld_basic() throws Exception {
+		
+		// Call GET "/hello-world" rest controller
+		RequestBuilder request = MockMvcRequestBuilders
+				.get("/hello-world")
+				.accept(MediaType.APPLICATION_JSON);
+		MvcResult result = mockMvc.perform(request).andReturn();
+		
+		// Verify the response
+		// System.out.println("Response body: " + result.getResponse().getContentAsString());
+		assertEquals("Hello World", result.getResponse().getContentAsString());
+		
+	}
+
+}
